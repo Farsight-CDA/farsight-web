2d28370102adba5f67550e0a2265e052a53be33d
leonlolly <leonlolly60952@gmail.com>
2022-10-14 22:35:33 +0200

    add edit functionality

--- src/__mocks__/chains.js
+++ src/__mocks__/chains.js
@@ -1,22 +1,22 @@
 export const chains = [
   {
     available: true,
-    content: [{ chainId: "137", owner: "fggte", expiration: 345476765, isKeeper: true }],
+    content: [{ chainId: 137, owner: "fggte", expiration: 345476765, isKeeper: true }],
   },
   {
     available: true,
-    content: [{ chainId: "137", owner: "ertgb", expiration: 456566577657657, isKeeper: false }],
+    content: [{ chainId: 137, owner: "ertgb", expiration: 456566577657657, isKeeper: false }],
   },
   {
     available: true,
-    content: [{ chainId: "56", owner: "fvw", expiration: 765959765, isKeeper: false }],
+    content: [{ chainId: 56, owner: "fvw", expiration: 765959765, isKeeper: false }],
   },
   {
     available: true,
-    content: [{ chainId: "97", owner: "wevf", expiration: 1665774772081, isKeeper: false }],
+    content: [{ chainId: 97, owner: "wevf", expiration: 1665774772081, isKeeper: false }],
   },
   {
     available: true,
-    content: [{ chainId: "80001", owner: "wefvw", expiration: 566786685768755, isKeeper: false }],
+    content: [{ chainId: 80001, owner: "wefvw", expiration: 566786685768755, isKeeper: false }],
   },
 ];
