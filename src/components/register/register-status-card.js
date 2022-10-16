104de047b5606eed2d3ab3a6a61c38289e30d1ba
leonlolly <leonlolly60952@gmail.com>
2022-10-16 20:32:03 +0200

    change margins

--- src/components/register/register-status-card.js
+++ src/components/register/register-status-card.js
@@ -2,15 +2,8 @@ import { useState, useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../../contexts/auth-context";
 import { mainChainId, isSupported } from "../../utils/chain-ids";
 import { commitment, namehash } from "../../utils/hash";
-import {
-  Button,
-  Step,
-  StepContent,
-  StepLabel,
-  Stepper,
-  Typography
-} from "@mui/material";
-import LinearProgress from '@mui/material/LinearProgress';
+import { Button, Step, StepContent, StepLabel, Stepper, Typography } from "@mui/material";
+import LinearProgress from "@mui/material/LinearProgress";
 import { func } from "prop-types";
 
 const minCommitmentAge = 180;
@@ -22,16 +15,24 @@ export const RegisterStatusCard = ({ name, duration }) => {
   const rerenderRef = useRef();
   rerenderRef.current = _r;
 
-  const [secret, _s] = useState("0x0000000000000000000000000000000000000000000000000000006d6168616d"); //ToDo: Generate random bytes
-
   const [activeStep, setActiveStep] = useState(0);
   const [waitingForTx, setWaitingForTx] = useState(false);
 
-  const { isConnected, address, chainId, controller, paymentProvider, paymentToken } = useContext(AuthContext);
+  const {
+    isConnected,
+    address,
+    chainId,
+    controller,
+    paymentProvider,
+    paymentToken,
+    secret,
+    ResetSecret,
+  } = useContext(AuthContext);
   const isSupportedChain = isSupported(chainId);
-  const commitmentHash = (name != null && address != null)
-    ? commitment(BigInt(namehash(name)), address, duration, secret)
-    : null;
+  const commitmentHash =
+    name != null && address != null
+      ? commitment(BigInt(namehash(name)), address, duration, secret)
+      : null;
 
   //Step State
   const [price, setPrice] = useState(null);
@@ -42,9 +43,14 @@ export const RegisterStatusCard = ({ name, duration }) => {
   const hasBalance = price != null && balance >= price;
 
   const [commitmentTime, setCommitmentTime] = useState(null);
-  const hasCommitment = commitmentTime != null && commitmentTime + maxCommitmentAge > new Date().getTime() / 1000;
-  const waitProgress = Math.min(100, 100 * (new Date().getTime() / 1000 - commitmentTime) / minCommitmentAge);
-  const waitCompleted = commitmentTime != null && commitmentTime + minCommitmentAge < new Date().getTime() / 1000;
+  const hasCommitment =
+    commitmentTime != null && commitmentTime + maxCommitmentAge > new Date().getTime() / 1000;
+  const waitProgress = Math.min(
+    100,
+    (100 * (new Date().getTime() / 1000 - commitmentTime)) / minCommitmentAge
+  );
+  const waitCompleted =
+    commitmentTime != null && commitmentTime + minCommitmentAge < new Date().getTime() / 1000;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -57,7 +63,7 @@ export const RegisterStatusCard = ({ name, duration }) => {
 
   useEffect(() => {
     updateSteps();
-  }, [isConnected, chainId, activeStep, approvedBalance, balance, commitmentTime])
+  }, [isConnected, chainId, activeStep, approvedBalance, balance, commitmentTime]);
 
   useEffect(() => {
     loadInitialValues();
@@ -113,7 +119,10 @@ export const RegisterStatusCard = ({ name, duration }) => {
     setWaitingForTx(true);
 
     try {
-      const receipt = await paymentToken.approve(paymentProvider.address, 100000000000000000000000000000n);
+      const receipt = await paymentToken.approve(
+        paymentProvider.address,
+        100000000000000000000000000000n
+      );
       const result = await receipt.wait();
 
       if (result.status != 1) {
@@ -122,8 +131,7 @@ export const RegisterStatusCard = ({ name, duration }) => {
       }
 
       setApprovedBalance(100000000000000000000000000000n);
-    }
-    finally {
+    } finally {
       setWaitingForTx(false);
     }
   }
@@ -145,8 +153,7 @@ export const RegisterStatusCard = ({ name, duration }) => {
       }
 
       setCommitmentTime(new Date().getTime() / 1000);
-    }
-    finally {
+    } finally {
       setWaitingForTx(false);
     }
   }
@@ -167,10 +174,9 @@ export const RegisterStatusCard = ({ name, duration }) => {
         alert("There was an error, please try again!");
         return;
       }
-
+      ResetSecret();
       location.reload();
-    }
-    finally {
+    } finally {
       setWaitingForTx(false);
     }
   }
@@ -179,85 +185,71 @@ export const RegisterStatusCard = ({ name, duration }) => {
     <Stepper activeStep={activeStep} orientation="vertical">
       <Step>
         <StepLabel>
-          <Typography variant="caption">
-            Connect your Wallet to a supported Chain
-          </Typography>
+          <Typography variant="caption">Connect your Wallet to a supported Chain</Typography>
         </StepLabel>
         <StepContent>
           {!isConnected && <Typography>Wallet not connected!</Typography>}
-          {(isConnected && !isSupportedChain) && <Typography>Unsupported Chain!</Typography>}
+          {isConnected && !isSupportedChain && <Typography>Unsupported Chain!</Typography>}
         </StepContent>
       </Step>
 
       <Step>
         <StepLabel>
-          Approve Farsight for USDC
+          <Typography variant="caption">Approve Farsight for USDC</Typography>
         </StepLabel>
         <StepContent>
-          {!isApproved && <><h1>Not Approved!</h1><Button
-            disabled={waitingForTx}
-            onClick={postApproveTransaction}
-            variant="contained"
-          >
-            Approve
-          </Button></>}
+          {!isApproved && (
+            <>
+              <h1>Not Approved!</h1>
+              <Button disabled={waitingForTx} onClick={postApproveTransaction} variant="contained">
+                Approve
+              </Button>
+            </>
+          )}
 
-          {!hasBalance && <Typography>
-            Your balance is too low!
-          </Typography>}
+          {!hasBalance && <Typography>Your balance is too low!</Typography>}
         </StepContent>
       </Step>
 
       <Step>
         <StepLabel>
-          <Typography variant="caption">
-            Commit to your registration On-Chain
-          </Typography>
+          <Typography variant="caption">Commit to your registration On-Chain</Typography>
         </StepLabel>
         <StepContent>
-          <Typography>
-            Accept the transaction in your wallet
-          </Typography>
-          <Button
-            disabled={waitingForTx }
-            onClick={postCommitTransaction}
-            variant="contained"
-          >
+          <Typography variant="caption">Accept the transaction in your wallet</Typography>
+          <Button disabled={waitingForTx} onClick={postCommitTransaction} variant="contained">
             Commit
           </Button>
         </StepContent>
       </Step>
       <Step>
         <StepLabel>
-          Wait 3 Minutes
+          <Typography variant="caption">
+            Commit to your registration On-Chain Wait 3 Minutes
+          </Typography>
         </StepLabel>
         <StepContent>
           <Typography>
-            Please hang tight, you will be able to finalize your registration shortly.
-            This wait time ensures that no matter which chain you are buying the domain from,
-            there is always enough time for you to register without being front-run
+            Please hang tight, you will be able to finalize your registration shortly. This wait
+            time ensures that no matter which chain you are buying the domain from, there is always
+            enough time for you to register without being front-run
           </Typography>
 
-          <LinearProgress variant="determinate" value={waitProgress} />
+          <LinearProgress sx={{ mt: 2, mb: 2 }} variant="determinate" value={waitProgress} />
 
-          <Button
-            disabled={!waitCompleted}
-            onClick={() => setActiveStep(4)}
-            variant="contained"
-          >
+          <Button disabled={!waitCompleted} onClick={() => setActiveStep(4)} variant="contained">
             Continue
           </Button>
         </StepContent>
       </Step>
       <Step>
         <StepLabel>
-          Complete Registration
+          <Typography variant="caption">Complete Registration</Typography>
         </StepLabel>
         <StepContent>
-          <Typography>
-            This step will claim the name NFT to your wallet.
-          </Typography>
+          <Typography>This step will claim the name NFT to your wallet.</Typography>
           <Button
+            sx={{ mt: 2, mb: 2 }}
             disabled={waitingForTx}
             onClick={postRegisterTransaction}
             variant="contained"
@@ -268,4 +260,4 @@ export const RegisterStatusCard = ({ name, duration }) => {
       </Step>
     </Stepper>
   );
-}
+};
