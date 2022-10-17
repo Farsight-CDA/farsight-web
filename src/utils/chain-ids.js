29899f5083b7dccd5d19684e0c2d9e1d9b5b9430
Playwo <eliaswolf2001@t-online.de>
2022-10-17 12:52:41 +0200

    Change network to mainnet

--- src/utils/chain-ids.js
+++ src/utils/chain-ids.js
@@ -2,18 +2,17 @@ export const polygonChainId = 137;
 export const fantomChainId = 250;
 export const bscChainId = 56;
 
-//export const isSupported = chainId => {
-//  return chainId === polygonChainId
-//    || chainId === fantomChainId
-//    || chainId === bscChainId;
-//}
+export const isSupported = chainId => {
+  return chainId === polygonChainId
+    || chainId === fantomChainId;
+}
 
 
 export const fantomTestnetChainId = 4002;
 
-export const isSupported = chainId => {
-  return chainId === fantomTestnetChainId;
-}
+//export const isSupported = chainId => {
+//  return chainId === fantomTestnetChainId;
+//}
 
 
-export const mainChainId = fantomTestnetChainId;
+export const mainChainId = polygonChainId;
