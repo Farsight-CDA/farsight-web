d9e91ea7122c0a8b2fb4e7effc4fb1a4a0fe4fbe
leonlolly <leonlolly60952@gmail.com>
2022-10-24 19:31:49 +0200

    style changed

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -87,116 +87,45 @@ export const theme = createTheme({
           margin: 0,
           padding: 0,
         },
-        html: {
-          MozOsxFontSmoothing: "grayscale",
-          WebkitFontSmoothing: "antialiased",
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100%",
-          width: "100%",
-        },
-        body: {
-          display: "flex",
-          flex: "1 1 auto",
-          flexDirection: "column",
-          minHeight: "100%",
-          width: "100%",
-        },
-        "#__next": {
-          display: "flex",
-          flex: "1 1 auto",
-          flexDirection: "column",
-          height: "100%",
-          width: "100%",
-        },
-      },
-    },
-    MuiOutlinedInput: {
-      styleOverrides: {
-        notchedOutline: {
-          borderColor: "#E6E8F0",
-        },
-      },
-    },
-    MuiTableHead: {
-      styleOverrides: {
-        root: {
-          backgroundColor: "#F3F4F6",
-          ".MuiTableCell-root": {
-            color: "#374151",
-          },
-          borderBottom: "none",
-          "& .MuiTableCell-root": {
-            borderBottom: "none",
-            fontSize: "12px",
-            fontWeight: 600,
-            lineHeight: 1,
-            letterSpacing: 0.5,
-            textTransform: "uppercase",
-          },
-          "& .MuiTableCell-paddingCheckbox": {
-            paddingTop: 4,
-            paddingBottom: 4,
-          },
-        },
       },
     },
   },
   palette: {
-    action: {
-      active: "#6B7280",
-      focus: "rgba(55, 65, 81, 0.12)",
-      hover: "rgba(55, 65, 81, 0.04)",
-      selected: "rgba(55, 65, 81, 0.08)",
-      disabledBackground: "rgba(55, 65, 81, 0.12)",
-      disabled: "rgba(55, 65, 81, 0.26)",
-    },
+    mode: "dark",
     background: {
-      default: "#E5E7EB",
-      paper: "#FFFFFF",
-    },
-    divider: "#E6E8F0",
-    primary: {
-      main: "#5048E5",
-      light: "#828DF8",
-      dark: "#3832A0",
-      contrastText: "#FFFFFF",
-    },
-    secondary: {
-      main: "#10B981",
-      light: "#3FC79A",
-      dark: "#0B815A",
-      contrastText: "#FFFFFF",
-    },
-    success: {
-      main: "#14B8A6",
-      light: "#43C6B7",
-      dark: "#0E8074",
-      contrastText: "#FFFFFF",
-    },
-    info: {
-      main: "#2196F3",
-      light: "#64B6F7",
-      dark: "#0B79D0",
-      contrastText: "#FFFFFF",
-    },
-    warning: {
-      main: "#FFB020",
-      light: "#FFBF4C",
-      dark: "#B27B16",
-      contrastText: "#FFFFFF",
-    },
-    error: {
-      main: "#D14343",
-      light: "#DA6868",
-      dark: "#922E2E",
-      contrastText: "#FFFFFF",
+      default: "#303030",
+      paper: "#424242",
     },
     text: {
-      primary: "#121828",
-      secondary: "#65748B",
-      disabled: "rgba(55, 65, 81, 0.48)",
+      primary: "#fff",
+      secondary: "rgba(255, 255, 255, 0.7)",
+      disabled: "rgba(255, 255, 255, 0.5)",
     },
+    primary: {
+      main: "#15a264",
+      light: "rgb(67, 180, 131)",
+      dark: "rgb(14, 113, 70)",
+    },
+    secondary: {
+      main: "#ea5d9b",
+      light: "rgb(238, 125, 175)",
+      dark: "rgb(163, 65, 108)",
+    },
+    error: {
+      main: "#f44336",
+    },
+    warning: {
+      main: "#ff9800",
+    },
+    info: {
+      main: "#2196f3",
+    },
+    success: {
+      main: "#4caf50",
+      light: "#81c784",
+      dark: "#388e3c",
+    },
+    divider: "rgba(255, 255, 255, 0.12)",
   },
   shape: {
     borderRadius: 8,
@@ -233,68 +162,6 @@ export const theme = createTheme({
       fontWeight: 600,
     },
     fontFamily:
-      '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
-    body1: {
-      fontSize: "1rem",
-      fontWeight: 400,
-      lineHeight: 1.5,
-    },
-    body2: {
-      fontSize: "0.875rem",
-      fontWeight: 400,
-      lineHeight: 1.57,
-    },
-    subtitle1: {
-      fontSize: "1rem",
-      fontWeight: 500,
-      lineHeight: 1.75,
-    },
-    subtitle2: {
-      fontSize: "0.875rem",
-      fontWeight: 500,
-      lineHeight: 1.57,
-    },
-    overline: {
-      fontSize: "0.75rem",
-      fontWeight: 600,
-      letterSpacing: "0.5px",
-      lineHeight: 2.5,
-      textTransform: "uppercase",
-    },
-    caption: {
-      fontSize: "0.75rem",
-      fontWeight: 400,
-      lineHeight: 1.66,
-    },
-    h1: {
-      fontWeight: 700,
-      fontSize: "3.5rem",
-      lineHeight: 1.375,
-    },
-    h2: {
-      fontWeight: 700,
-      fontSize: "3rem",
-      lineHeight: 1.375,
-    },
-    h3: {
-      fontWeight: 700,
-      fontSize: "2.25rem",
-      lineHeight: 1.375,
-    },
-    h4: {
-      fontWeight: 700,
-      fontSize: "2rem",
-      lineHeight: 1.375,
-    },
-    h5: {
-      fontWeight: 600,
-      fontSize: "1.5rem",
-      lineHeight: 1.375,
-    },
-    h6: {
-      fontWeight: 600,
-      fontSize: "1.125rem",
-      lineHeight: 1.375,
-    },
+      '"Quicksand","Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
   },
 });
