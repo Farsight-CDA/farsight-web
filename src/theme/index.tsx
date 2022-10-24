6a82cdd2f5ef2c7bf8d863ad8b7520df259b1d5a
leonlolly <leonlolly60952@gmail.com>
2022-10-24 22:47:24 +0200

    style changed

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -21,75 +21,6 @@ export const theme = createTheme({
       pc: 1024,
     },
   },
-  components: {
-    MuiButton: {
-      defaultProps: {
-        disableElevation: true,
-      },
-      styleOverrides: {
-        root: {
-          textTransform: "none",
-        },
-        sizeSmall: {
-          padding: "6px 16px",
-        },
-        sizeMedium: {
-          padding: "8px 20px",
-        },
-        sizeLarge: {
-          padding: "11px 24px",
-        },
-        textSizeSmall: {
-          padding: "7px 12px",
-        },
-        textSizeMedium: {
-          padding: "9px 16px",
-        },
-        textSizeLarge: {
-          padding: "12px 16px",
-        },
-      },
-    },
-    MuiButtonBase: {
-      defaultProps: {
-        disableRipple: true,
-      },
-    },
-    MuiCardContent: {
-      styleOverrides: {
-        root: {
-          padding: "32px 24px",
-          "&:last-child": {
-            paddingBottom: "32px",
-          },
-        },
-      },
-    },
-    MuiCardHeader: {
-      defaultProps: {
-        titleTypographyProps: {
-          variant: "h6",
-        },
-        subheaderTypographyProps: {
-          variant: "body2",
-        },
-      },
-      styleOverrides: {
-        root: {
-          padding: "32px 24px",
-        },
-      },
-    },
-    MuiCssBaseline: {
-      styleOverrides: {
-        "*": {
-          boxSizing: "border-box",
-          margin: 0,
-          padding: 0,
-        },
-      },
-    },
-  },
   palette: {
     mode: "dark",
     background: {
@@ -130,33 +61,6 @@ export const theme = createTheme({
   shape: {
     borderRadius: 8,
   },
-  shadows: [
-    "none",
-    "0px 1px 1px rgba(100, 116, 139, 0.06), 0px 1px 2px rgba(100, 116, 139, 0.1)",
-    "0px 1px 2px rgba(100, 116, 139, 0.12)",
-    "0px 1px 4px rgba(100, 116, 139, 0.12)",
-    "0px 1px 5px rgba(100, 116, 139, 0.12)",
-    "0px 1px 6px rgba(100, 116, 139, 0.12)",
-    "0px 2px 6px rgba(100, 116, 139, 0.12)",
-    "0px 3px 6px rgba(100, 116, 139, 0.12)",
-    "0px 2px 4px rgba(31, 41, 55, 0.06), 0px 4px 6px rgba(100, 116, 139, 0.12)",
-    "0px 5px 12px rgba(100, 116, 139, 0.12)",
-    "0px 5px 14px rgba(100, 116, 139, 0.12)",
-    "0px 5px 15px rgba(100, 116, 139, 0.12)",
-    "0px 6px 15px rgba(100, 116, 139, 0.12)",
-    "0px 7px 15px rgba(100, 116, 139, 0.12)",
-    "0px 8px 15px rgba(100, 116, 139, 0.12)",
-    "0px 9px 15px rgba(100, 116, 139, 0.12)",
-    "0px 10px 15px rgba(100, 116, 139, 0.12)",
-    "0px 12px 22px -8px rgba(100, 116, 139, 0.25)",
-    "0px 13px 22px -8px rgba(100, 116, 139, 0.25)",
-    "0px 14px 24px -8px rgba(100, 116, 139, 0.25)",
-    "0px 10px 10px rgba(31, 41, 55, 0.04), 0px 20px 25px rgba(31, 41, 55, 0.1)",
-    "0px 25px 50px rgba(100, 116, 139, 0.25)",
-    "0px 25px 50px rgba(100, 116, 139, 0.25)",
-    "0px 25px 50px rgba(100, 116, 139, 0.25)",
-    "0px 25px 50px rgba(100, 116, 139, 0.25)",
-  ],
   typography: {
     button: {
       fontWeight: 600,
